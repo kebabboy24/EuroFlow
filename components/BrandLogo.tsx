659198536e187266dab17e39f3883648c68c2c03
kebabboy24 <kebabboy24@gmail.com>
2026-07-16 19:47:05 +0300

    а

--- components/BrandLogo.tsx
+++ components/BrandLogo.tsx
@@ -3,7 +3,8 @@ import Link from "next/link";
 export default function BrandLogo() {
   return (
     <Link href="/" className="brand" aria-label="EuroFlow">
-      <img className="brand-logo-img" src="/brand/euroflow-logo.png" alt="EuroFlow" />
+      <img className="brand-logo-img brand-logo-light" src="/brand/euroflow-logo.png" alt="EuroFlow" />
+      <img className="brand-logo-img brand-logo-dark" src="/brand/euroflow-logo-dark.png" alt="" aria-hidden="true" />
     </Link>
   );
 }
